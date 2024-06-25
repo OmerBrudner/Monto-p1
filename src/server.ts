06721ba247a3a640205e851af321627e4de4d71f
Omer Brudner <128624990+OmerBrudner@users.noreply.github.com>
2024-06-25 15:51:22 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -30,37 +30,37 @@ const start = async () => {
   
     // Declaration of a route that returns a simple "Hello" message
 
-//   const helloQuerySchema = {
-//     type: 'object',
-//     properties: {
-//       name: { type: 'string' }
-//     },
-//     required: []
-//   };
+  const helloQuerySchema = {
+    type: 'object',
+    properties: {
+      name: { type: 'string' }
+    },
+    required: []
+  };
 
-//   fastify.get<{ Querystring: { name?: string } }>('/hello', {
-//     schema: {
-//       querystring: helloQuerySchema
-//     }
-//   }, async (request: FastifyRequest<{ Querystring: { name?: string } }>, reply: FastifyReply) => {
-//     try {
-//       const { name } = request.query;
-//       return { message: `Hello ${name || 'World'}` };
-//     } catch (error) {
-//       Sentry.captureException(error);
-//       reply.code(500).send({ error: 'An error occurred' });
-//     }
-//   });
+  fastify.get<{ Querystring: { name?: string } }>('/hello', {
+    schema: {
+      querystring: helloQuerySchema
+    }
+  }, async (request: FastifyRequest<{ Querystring: { name?: string } }>, reply: FastifyReply) => {
+    try {
+      const { name } = request.query;
+      return { message: `Hello ${name || 'World'}` };
+    } catch (error) {
+      Sentry.captureException(error);
+      reply.code(500).send({ error: 'An error occurred' });
+    }
+  });
 
     // decleration of a rout that throw an error
-// fastify.get('/error', async (request, reply) => {
-//     try {
-//         throw new Error('error!!!')
-//     } catch (error) {
-//         Sentry.captureException(error);
-//         reply.code(500).send({error: 'Error it is!'});
-//     }
-// });
+fastify.get('/error', async (request, reply) => {
+    try {
+        throw new Error('error!!!')
+    } catch (error) {
+        Sentry.captureException(error);
+        reply.code(500).send({error: 'Error it is!'});
+    }
+});
 
 // run the server
 
