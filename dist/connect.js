5fff2ccc92e004edb342b1039cc86b3f97620741
Omer Brudner <128624990+OmerBrudner@users.noreply.github.com>
2024-06-26 17:29:26 +0300

    Delete dist directory

--- dist/connect.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// import { MongoClient } from "mongodb";
-// import { dotenv} from "dotenv";
-// dotenv.config();
-// const uri = process.env.MONGO_URI as string;
-//# sourceMappingURL=connect.js.map
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const greeting = 'hello';
-console.log(greeting);
-console.log(process.env.DSN_SENTRY);
-//# sourceMappingURL=index.js.map
